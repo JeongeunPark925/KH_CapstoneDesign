48f3d0e0e0c8f42643fe6cdec9d572d5356f2838
JeongeunJPark <jeongeunswd@gmail.com>
2021-03-22 01:09:54 +0900

    3월 23일 실습을 위한 환경 배포

---
+++ 1일차_Setup/KH_Sample_VR_git/KH_SampleVR/Assets/8_Scripts/MoveControl.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl : MonoBehaviour
+{
+    //트랜스폼 배열을 points에 저장
+    public Transform[] points;
+
+    //컴포넌트 저장 변수
+    private Transform tr;
+    private int nextIndex = 1;
+
+    //움직이는 방법으로 
+    //1. 웨이포인트를 따라간다
+    //2. 쳐다보는 쪽으로 움직인다
+    //3. 데이드림(No!)
+    public enum MoveType
+    {
+        WAYPOINT,
+        LOOKAT,
+        DAYDREAM
+    }
+
+    //지금 움직이는 방식 = 웨이포인트 방식이다.
+    public MoveType moveType = MoveType.WAYPOINT;
+
+    //이동속도
+    public float moveSpeed = 2.0f;
+
+    //회전속도
+    public float damping = 3.0f;
+
+    public int lookatSpeed = 3;
+
+
+    //바라보는 방향으로 움직임
+    private CharacterController cc;
+
+    //메인카메라의 트랜스폼을 저장할 변수
+    private Transform mainCameraTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //기초 연결
+        tr = GetComponent<Transform>();
+
+        cc = GetComponent<CharacterController>();
+
+        mainCameraTransform = Camera.main.GetComponent<Transform>();
+
+
+        GameObject waypointGroup = GameObject.Find("WaypointGroup");
+
+        if(waypointGroup != null)
+        {
+            points = waypointGroup.GetComponentsInChildren<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (moveType)
+        {
+            case MoveType.WAYPOINT:
+                MoveWayPoint();
+                break;
+
+            case MoveType.LOOKAT:
+                MoveLookat(lookatSpeed);
+                break;
+
+            case MoveType.DAYDREAM:
+                break;
+        }
+    }
+
+    public void MoveWayPoint()
+    {
+        Vector3 direction = points[nextIndex].position - tr.position;
+
+        Quaternion userRotation = Quaternion.LookRotation(direction);
+
+        tr.rotation = Quaternion.Slerp(tr.rotation, userRotation, Time.deltaTime * damping);
+
+        tr.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+    }
+
+    public void MoveLookat(int facing)
+    {
+        Vector3 heading = mainCameraTransform.forward;
+
+        //지면 기준의 벡터로 전환(constraint)
+        heading.y = 0f;
+
+        Debug.DrawRay(tr.position, heading.normalized * 1.0f, Color.gray);
+
+        cc.SimpleMove(heading * moveSpeed * facing);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("WAYPOINT"))
+        {
+            //3항식
+            nextIndex = (++nextIndex >= points.Length) ? 1 : nextIndex;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+경희대 캡스톤 디자인을 위한 레포지토리입니다.
+
+[이번 알림] 3월 23일 실습 준비 안내
+
+1. 오른쪽 상단의 'Code' - DownloadZip 하여 '2일차_setup' 부분을 저장해주세요. 
+(이 안에 있는 .unitypackage 파일을 실습에서 사용할 예정입니다.)
+
+2. 2일차실습준비.jpg 를 읽고, Unity hub에서 Android build Support 부분을 추가설치 해주세요.
+(Android build support 하단의 Android SDK&NDK, OpenJDK를 모두 설치해야 합니다.)
+
+3. 지난 실습 시간에 따라오기 힘들었거나, 오류가 있었던 학생들을 위해 구글 드라이브에 KH_Sample_VR_2일차.zip을 만들어두었습니다.
+- 구글 드라이브 : https://drive.google.com/drive/u/1/folders/1vYO42Bw5HQ4sHEeNpoG_y1pMRD_XTGv7
+필요한 경우 다운받아 준비해주세요.
+
+4. 작업에 문제가 있을 때에는 jeongeunswd@gmail.com으로 문의 바랍니다.
+
+3월 23일 실습 시간에 뵙겠습니다. :)
+
+-----------------------------------------------------------------------
+
+[지난 알림] 3월 16일 실습 준비 안내 
+
+1. 오른쪽 상단의 'Code' 를 누른 뒤, DownloadZip 하여 다운로드한 파일을 실습할 노트북의 적당한 위치에 저장해주세요.
+
+2. 다운로드받은 zip 파일을 풀면, Setup 이라는 이름의 폴더가 있을 것입니다.
+
+3. 폴더 안의 VR_README를 읽고, 안내에 따라 프로그램 설치를 진행해주세요.
+
+- 1. 안드로이드, 자바 , 유니티 설치 파일은 이 링크에서 다운받으세요 
+- https://drive.google.com/drive/u/1/folders/1vYO42Bw5HQ4sHEeNpoG_y1pMRD_XTGv7
+
+- 2. 유니티 2019.4.17f 설치 파일을 구글 드라이브 링크에 추가해 두었습니다.
+
+- 3. 3-2의 설치파일이 열리지 않거나 문제가 있는 경우, https://unity3d.com/kr/unity/qa/lts-releases 페이지에서
+"LTS 릴리스 2019.4.171" 아래의 Unity 에디터 다운로드 도우미(Unity Hub)를 선택하여 유니티 허브에서 2019.4.17f를 설치하십시오.
+
+4. 설치작업에는 시간이 좀 걸리므로, 여유있는 시간대에 하시는 것을 권장드립니다.
+
+5. 혹시 설치 파일에 오류가 있거나 작업에 문제가 있을 때에는 jeongeunswd@gmail.com으로 문의 바랍니다.
+
